01b0cc6e46df7d2998036979e59a9ab457cc0561
Xueru Fan <xueru@mail.bnu.edu.cn>
2024-06-16 13:05:39 +0800

    Update 43_A_PlotAfterSpectralCluster34DK.R
    # Part 1: 画出两个分型的34个分区体积centile的概率密度分布图，png
    # Part 2: 画出两个分型7个全局指标常模分（中位数+四分位距）的概率密度图，png
    # Part 3: 画出两个分型的34个脑区的异常（5%和95%）流行率，png和csv

--- 43_A_PlotAfterSpectralCluster34DK.R
+++ 43_A_PlotAfterSpectralCluster34DK.R
@@ -4,9 +4,7 @@
 ###################################################
 # Part 1: 画出两个分型的34个分区体积centile的概率密度分布图，png
 # Part 2: 画出两个分型7个全局指标常模分（中位数+四分位距）的概率密度图，png
-# Part 3: 画出两个分型的34个分区体积centile中位数投射出的脑图，png
-# Part 4: 画出两个分型的34个脑区的异常（5%和95%）流行率，png和csv
-
+# Part 3: 画出两个分型的34个脑区的异常（5%和95%）流行率，png和csv
 ###################################################
 
 rm(list=ls())
@@ -25,11 +23,11 @@ name <- paste0("abide_A_asd_male_dev_Spectral_Cluster_34DK_", newDate, ".csv")
 cluster_result <- read.csv(file.path(resDir, name))
 
 id_group <- c("1", "2") # cluster 1 是L， 2是H
-group1 <- (subset(cluster_result, clusterID == "1"))[, -1:-2]
-group2 <- (subset(cluster_result, clusterID == "2"))[, -1:-2]
+
 
 ######################### Part 1: Project 34 region centile on the brain ###########################
-
+group1 <- (subset(cluster_result, clusterID == "1"))[, -1:-9]
+group2 <- (subset(cluster_result, clusterID == "2"))[, -1:-9]
 ######################### cluster 1
 asd_parc <- group1
 median <- apply(asd_parc, 2, median, na.rm = TRUE)
@@ -75,13 +73,14 @@ ggseg(.data = asd_parc_centile, mapping = aes(fill = median), color = "black", a
 name <- paste0("abide_A_asd_male_dev_Spectral_Cluster_34DK_2_Centile_Brain_", newDate, ".png")
 ggsave(file.path(plotDir, name), width = 7.8, height = 3, units = "in", dpi = 500)
 
-objects_to_keep <- c("plotDir", "abideDir", "dataDir", "newDate", "cluster_result", "group1",
-                     "group2", "resDir")
+objects_to_keep <- c("plotDir", "abideDir", "dataDir", "newDate", "cluster_result", "resDir",
+                     "id_group")
 rm(list = (setdiff(ls(), objects_to_keep)))
 
 
 ######################### Part 2: Plot density for 7 global ########################################
-
+# 初始化空数据框
+all_data <- data.frame()
 for (id in id_group){
   # id <- "1"
   eval(parse(text = paste0("group", id, " <- (subset(cluster_result, clusterID == ", id, "))[, -1:-2]")))
@@ -89,99 +88,44 @@ for (id in id_group){
   global <- global[which(names(global) == "GMV"):which(names(global) == "totalSA2")]
   asd_long <- gather(global, key = "Measure", value = "Centile", GMV:totalSA2, na.rm = TRUE,
                      factor_key = TRUE)
-  centile <- as.numeric(asd_long$Centile)
-  measure <- asd_long$Measure
-  measure2 <- reorder(measure, centile, FUN = median) # from small to large
-  # modify name
-  levels(measure2)[levels(measure2) == "GMV"] <- "Grey Matter Volume"
-  levels(measure2)[levels(measure2) == "WMV"] <- "White Matter Volume"
-  levels(measure2)[levels(measure2) == "sGMV"] <- "Subcortical Volume"
-  levels(measure2)[levels(measure2) == "TCV"] <- "Total Cerebrum Volume"
-  levels(measure2)[levels(measure2) == "meanCT2"] <- "Mean Cortical Thickness"
-  levels(measure2)[levels(measure2) == "Ventricles"] <- "Ventricular Volume"
-  levels(measure2)[levels(measure2) == "totalSA2"] <- "Total Surface Area"
-  
-  ggplot(asd_long, aes(x = centile, y = measure2, fill = measure2)) +
-    geom_density_ridges(scale = 1.8, quantile_lines = TRUE, size = 0.9, quantiles = 4) +
-    scale_x_continuous(breaks = seq(0, 1, by = 0.25), labels = c("0%", "25%", "50%", "75%", "100%")) +
-    scale_fill_manual(values = c("Grey Matter Volume" = "#4682b4",
-                                 "White Matter Volume" = "#dcdcdc",
-                                 "Subcortical Volume" = "#808080",
-                                 "Total Cerebrum Volume" = "#f0e68c",
-                                 "Mean Cortical Thickness" = "#008b8b",
-                                 "Total Surface Area" = "#ffa07a",
-                                 "Ventricular Volume" = "#ffb6c1")) +
-    coord_fixed(ratio = 0.2) + 
-    xlab("") +
-    ylab("") +
-    theme_ridges() + 
-    theme(legend.position = "none", # without legend
-          axis.text.y = element_text(size = 10, face = "bold"),
-          axis.text.x = element_text(size = 10, face = "bold"))
-  name <- paste0("abide_A_asd_male_dev_Spectral_Cluster_34DK_", id, "_Centile_Global_", newDate, ".png")
-  ggsave(file.path(plotDir, name), width = 6, height = 5, units = "in", dpi = 500)
-  
+  asd_long$Group <- factor(rep(id, nrow(asd_long)))  # 添加群体标识
+  all_data <- rbind(all_data, asd_long)  # 合并数据
 }
+  
+# 修改度量标准的显示名称
+all_data$Measure <- factor(all_data$Measure, levels = c("TCV", "WMV", "GMV", "sGMV", "Ventricles",
+                                                        "meanCT2", "totalSA2"),
+                           labels = c("Total Cerebrum Volume", "White Matter Volume",
+                                      "Grey Matter Volume", "Subcortical Volume",
+                                      "Ventricular Volume", "Mean Cortical Thickness",
+                                      "Total Surface Area"))
+
+ggplot(all_data, aes(x = Centile, y = Measure, fill = Measure, group = interaction(Group, Measure))) +
+  geom_density_ridges(scale = 1.6, quantile_lines = TRUE, size = 1, quantiles = 2, alpha = .9) +
+  scale_x_continuous(breaks = seq(0, 1, by = 0.25), labels = c("0%", "25%", "50%", "75%", "100%")) +
+  scale_fill_manual(values = c("Grey Matter Volume" = "#4682b4",
+                               "White Matter Volume" = "#dcdcdc",
+                               "Subcortical Volume" = "#808080",
+                               "Total Cerebrum Volume" = "#f0e68c",
+                               "Mean Cortical Thickness" = "#008b8b",
+                               "Total Surface Area" = "#ffa07a",
+                               "Ventricular Volume" = "#ffb6c1")) +
+  coord_fixed(ratio = 0.2) + 
+  xlab("") +
+  ylab("") +
+  theme_ridges() + 
+  theme(legend.position = "none", # without legend
+        axis.text.y = element_text(size = 10, face = "bold"),
+        axis.text.x = element_text(size = 10, face = "bold"))
+name <- paste0("abide_A_asd_male_dev_Spectral_Cluster_34DK_Centile_Global_", newDate, ".png")
+ggsave(file.path(plotDir, name), width = 6, height = 5, units = "in", dpi = 500)
+
 
 objects_to_keep <- c("plotDir", "abideDir", "dataDir", "resDir", "newDate", "cluster_result")
 rm(list = (setdiff(ls(), objects_to_keep)))
 
 
-######################### Part 3: Project 34 region centile on the brain ###########################
-
-
-######################### cluster 1
-asd_parc <- group1
-median <- apply(asd_parc, 2, median, na.rm = TRUE)
-
-label <- names(asd_parc)[8:41]
-asd_parc_centile <- data.frame(label)
-asd_parc_centile$median <- median[8:41]
-asd_parc_centile$label <- paste0("lh_", asd_parc_centile$label)
-
-median <- as.numeric(asd_parc_centile$median)
-label <- as.character(asd_parc_centile$label)
-
-ggseg(.data = asd_parc_centile, mapping = aes(fill = median), color = "black", atlas = dk,
-      hemisphere = "left", size = 1.2) +
-  theme_void() +
-  theme(legend.title = element_blank(), legend.position = "bottom",
-        legend.key.width = unit(1, "cm")) +
-  scale_fill_gradient(low = "#0064b5", high = "white", limits = c(min(median), max(median))) +
-  guides(fill = guide_colourbar(frame.colour = "black", frame.linewidth = 1, ticks = FALSE))
-
-name <- paste0("abide_A_asd_male_dev_Spectral_Cluster_34DK_1_Centile_Brain_", newDate, ".png")
-ggsave(file.path(plotDir, name), width = 7.8, height = 3, units = "in", dpi = 500)
-
-
-######################### cluster 2
-asd_parc <- group2
-median <- apply(asd_parc, 2, median, na.rm = TRUE)
-label <- names(asd_parc)[8:41]
-asd_parc_centile <- data.frame(label)
-asd_parc_centile$median <- median[8:41]
-asd_parc_centile$label <- paste0("lh_", asd_parc_centile$label)
-median <- as.numeric(asd_parc_centile$median)
-label <- as.character(asd_parc_centile$label)
-
-ggseg(.data = asd_parc_centile, mapping = aes(fill = median), color = "black", atlas = dk,
-      position = "stacked", hemisphere = "left", size = 1.2) +
-  theme_void() +
-  theme(legend.title = element_blank(), legend.position = "bottom",
-        legend.key.width = unit(1, "cm")) +
-  scale_fill_gradient(low = "white", high = "#ff6347", limits = c(min(median), max(median))) +
-  guides(fill = guide_colourbar(frame.colour = "black", frame.linewidth = 1, ticks = FALSE))
-
-name <- paste0("abide_A_asd_male_dev_Spectral_Cluster_2_34DK_Centile_Brain_", newDate, ".png")
-ggsave(file.path(plotDir, name), width = 7.8, height = 3, units = "in", dpi = 500)
-
-objects_to_keep <- c("plotDir", "abideDir", "dataDir", "newDate", "cluster_result", "group1",
-                     "group2", "resDir")
-rm(list = (setdiff(ls(), objects_to_keep)))
-
-
-
-######################### Part 4: Project extram percentage on the brain ###########################
+######################### Part 3: Project extram percentage on the brain ###########################
 # 34个脑区的异常流行率, 对于每个脑区来说有多少人的体积常模分属于极端值（位于thr以外，这里thr取5%）
 
 thr <- 0.05
