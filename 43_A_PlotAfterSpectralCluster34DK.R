107b8c4abaf6c6487fe4e7d3f4eac83d0c01a9be
Xueru Fan <xueru@mail.bnu.edu.cn>
2024-06-16 12:28:51 +0800

    Update 43_A_PlotAfterSpectralCluster34DK.R
    # Part 0: 对每个特征值进行标准化，聚类时时使用标准化的centile
    # Part 1: 选择不同聚类数的轮廓系数，确定最佳聚类数目，png和csv
    # Part 2: 根据最佳聚类数目进行谱聚类，保存结果csv，保存了cluster编号和41个脑指标的原始centile
    # Part 3: 使用特征消除法评估每个特征的贡献，保存排序的png和csv

--- 43_A_PlotAfterSpectralCluster34DK.R
+++ 43_A_PlotAfterSpectralCluster34DK.R
@@ -6,6 +6,7 @@
 # Part 2: 画出两个分型7个全局指标常模分（中位数+四分位距）的概率密度图，png
 # Part 3: 画出两个分型的34个分区体积centile中位数投射出的脑图，png
 # Part 4: 画出两个分型的34个脑区的异常（5%和95%）流行率，png和csv
+
 ###################################################
 
 rm(list=ls())
@@ -24,12 +25,10 @@ name <- paste0("abide_A_asd_male_dev_Spectral_Cluster_34DK_", newDate, ".csv")
 cluster_result <- read.csv(file.path(resDir, name))
 
 id_group <- c("1", "2") # cluster 1 是L， 2是H
-
-
-######################### Part 1: Project 34 region centile on the brain ###########################
-
 group1 <- (subset(cluster_result, clusterID == "1"))[, -1:-2]
 group2 <- (subset(cluster_result, clusterID == "2"))[, -1:-2]
+
+######################### Part 1: Project 34 region centile on the brain ###########################
 
 ######################### cluster 1
 asd_parc <- group1
@@ -79,6 +78,107 @@ ggsave(file.path(plotDir, name), width = 7.8, height = 3, units = "in", dpi = 50
 objects_to_keep <- c("plotDir", "abideDir", "dataDir", "newDate", "cluster_result", "group1",
                      "group2", "resDir")
 rm(list = (setdiff(ls(), objects_to_keep)))
+
+
+######################### Part 2: Plot density for 7 global ########################################
+
+for (id in id_group){
+  # id <- "1"
+  eval(parse(text = paste0("group", id, " <- (subset(cluster_result, clusterID == ", id, "))[, -1:-2]")))
+  eval(parse(text = paste0("global <- group", id)))
+  global <- global[which(names(global) == "GMV"):which(names(global) == "totalSA2")]
+  asd_long <- gather(global, key = "Measure", value = "Centile", GMV:totalSA2, na.rm = TRUE,
+                     factor_key = TRUE)
+  centile <- as.numeric(asd_long$Centile)
+  measure <- asd_long$Measure
+  measure2 <- reorder(measure, centile, FUN = median) # from small to large
+  # modify name
+  levels(measure2)[levels(measure2) == "GMV"] <- "Grey Matter Volume"
+  levels(measure2)[levels(measure2) == "WMV"] <- "White Matter Volume"
+  levels(measure2)[levels(measure2) == "sGMV"] <- "Subcortical Volume"
+  levels(measure2)[levels(measure2) == "TCV"] <- "Total Cerebrum Volume"
+  levels(measure2)[levels(measure2) == "meanCT2"] <- "Mean Cortical Thickness"
+  levels(measure2)[levels(measure2) == "Ventricles"] <- "Ventricular Volume"
+  levels(measure2)[levels(measure2) == "totalSA2"] <- "Total Surface Area"
+  
+  ggplot(asd_long, aes(x = centile, y = measure2, fill = measure2)) +
+    geom_density_ridges(scale = 1.8, quantile_lines = TRUE, size = 0.9, quantiles = 4) +
+    scale_x_continuous(breaks = seq(0, 1, by = 0.25), labels = c("0%", "25%", "50%", "75%", "100%")) +
+    scale_fill_manual(values = c("Grey Matter Volume" = "#4682b4",
+                                 "White Matter Volume" = "#dcdcdc",
+                                 "Subcortical Volume" = "#808080",
+                                 "Total Cerebrum Volume" = "#f0e68c",
+                                 "Mean Cortical Thickness" = "#008b8b",
+                                 "Total Surface Area" = "#ffa07a",
+                                 "Ventricular Volume" = "#ffb6c1")) +
+    coord_fixed(ratio = 0.2) + 
+    xlab("") +
+    ylab("") +
+    theme_ridges() + 
+    theme(legend.position = "none", # without legend
+          axis.text.y = element_text(size = 10, face = "bold"),
+          axis.text.x = element_text(size = 10, face = "bold"))
+  name <- paste0("abide_A_asd_male_dev_Spectral_Cluster_34DK_", id, "_Centile_Global_", newDate, ".png")
+  ggsave(file.path(plotDir, name), width = 6, height = 5, units = "in", dpi = 500)
+  
+}
+
+objects_to_keep <- c("plotDir", "abideDir", "dataDir", "resDir", "newDate", "cluster_result")
+rm(list = (setdiff(ls(), objects_to_keep)))
+
+
+######################### Part 3: Project 34 region centile on the brain ###########################
+
+
+######################### cluster 1
+asd_parc <- group1
+median <- apply(asd_parc, 2, median, na.rm = TRUE)
+
+label <- names(asd_parc)[8:41]
+asd_parc_centile <- data.frame(label)
+asd_parc_centile$median <- median[8:41]
+asd_parc_centile$label <- paste0("lh_", asd_parc_centile$label)
+
+median <- as.numeric(asd_parc_centile$median)
+label <- as.character(asd_parc_centile$label)
+
+ggseg(.data = asd_parc_centile, mapping = aes(fill = median), color = "black", atlas = dk,
+      hemisphere = "left", size = 1.2) +
+  theme_void() +
+  theme(legend.title = element_blank(), legend.position = "bottom",
+        legend.key.width = unit(1, "cm")) +
+  scale_fill_gradient(low = "#0064b5", high = "white", limits = c(min(median), max(median))) +
+  guides(fill = guide_colourbar(frame.colour = "black", frame.linewidth = 1, ticks = FALSE))
+
+name <- paste0("abide_A_asd_male_dev_Spectral_Cluster_34DK_1_Centile_Brain_", newDate, ".png")
+ggsave(file.path(plotDir, name), width = 7.8, height = 3, units = "in", dpi = 500)
+
+
+######################### cluster 2
+asd_parc <- group2
+median <- apply(asd_parc, 2, median, na.rm = TRUE)
+label <- names(asd_parc)[8:41]
+asd_parc_centile <- data.frame(label)
+asd_parc_centile$median <- median[8:41]
+asd_parc_centile$label <- paste0("lh_", asd_parc_centile$label)
+median <- as.numeric(asd_parc_centile$median)
+label <- as.character(asd_parc_centile$label)
+
+ggseg(.data = asd_parc_centile, mapping = aes(fill = median), color = "black", atlas = dk,
+      position = "stacked", hemisphere = "left", size = 1.2) +
+  theme_void() +
+  theme(legend.title = element_blank(), legend.position = "bottom",
+        legend.key.width = unit(1, "cm")) +
+  scale_fill_gradient(low = "white", high = "#ff6347", limits = c(min(median), max(median))) +
+  guides(fill = guide_colourbar(frame.colour = "black", frame.linewidth = 1, ticks = FALSE))
+
+name <- paste0("abide_A_asd_male_dev_Spectral_Cluster_2_34DK_Centile_Brain_", newDate, ".png")
+ggsave(file.path(plotDir, name), width = 7.8, height = 3, units = "in", dpi = 500)
+
+objects_to_keep <- c("plotDir", "abideDir", "dataDir", "newDate", "cluster_result", "group1",
+                     "group2", "resDir")
+rm(list = (setdiff(ls(), objects_to_keep)))
+
 
 
 ######################### Part 4: Project extram percentage on the brain ###########################
