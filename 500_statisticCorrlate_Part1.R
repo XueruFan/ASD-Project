79c5fbc64f6cf4dd0310b01881b7b2ecb64322c7
Xueru Fan <xueru@mail.bnu.edu.cn>
2024-08-02 17:30:53 +0800

    update

--- 500_statisticCorrlate_Part1.R
+++ 500_statisticCorrlate_Part1.R
@@ -1,9 +1,14 @@
-# 本代码用来分析两组ASD男性的变量之间的相关系数和显著性水平
-# 斯皮尔曼相关，Site和FIQ作为控制变量，全脑指标合成的新指标作为自变量,
-# 保存原始的相关系数和p值csv文件，另外，筛选p小于0.01且r大于0.2的结果，保存csv文件
-# 之后手动筛选保存一个xlsx
-# 按照显著性水平结果，绘制相关图png
+# 本代码用来分析两组ASD发育男性的变量之间的相关系数和显著性水平
+# 使用斯皮尔曼相关，Site和FIQ作为控制变量
 # 雪如 2024年2月28日于北师大办公室
+##################################
+# Part 1: 34个局部脑指标（centile）加权合成的新指标作为自变量
+# 保存原始的相关系数和p值csv文件，另外，筛选p小于0.01且r大于0.2的结果，保存csv文件
+# 按照显著性水平结果，绘制相关图png
+
+# Part 2: 34个脑区中贡献top5的指标合成的新指标作为自变量
+# 保存原始的相关系数和p值csv文件，另外，筛选p小于0.01且r大于0.2的结果【无】
+##################################
 
 rm(list=ls())
 packages <- c("tidyverse", "mgcv", "stringr", "reshape2", "magrittr", "ggplot2", "dplyr", "readxl",
@@ -15,23 +20,20 @@ sapply(packages, require, character.only = TRUE)
 # abideDir <- '/Volumes/Xueru/PhDproject/ABIDE' # mac
 abideDir <- 'E:/PhDproject/ABIDE' # winds
 phenoDir <- file.path(abideDir, "Preprocessed")
-clustDir <- file.path(abideDir, "Analysis/Cluster/Cluster_A/SpectralCluster34DK")
+clustDir <- file.path(abideDir, "Analysis/Cluster/SpectralCluster")
 statiDir <- file.path(abideDir, "Analysis/Statistic")
-plotDir <- file.path(abideDir, "Plot/Cluster/Cluster_A/SpectralCluster34DK/Corr/CorrA")
+plotDir <- file.path(abideDir, "Plot/Cluster/SpectralCluster/Corr")
 resDate <- "240315"
 newDate <- "240610"
 
 # 认知行为
-pheno <- read.csv(file.path(phenoDir, paste0("abide_A_All_", resDate, ".csv")))
+pheno <- read.csv(file.path(phenoDir, paste0("abide_A_all_", resDate, ".csv")))
 colnames(pheno)[which(names(pheno) == "Participant")] <- "participant"
 # 聚类信息、脑形态测量百分位数
-name <- paste0("abide_A_asd_male_dev_Spectral_Cluster_34DK_", newDate, ".csv")
+name <- paste0("asd_male_Spectral_Cluster_", newDate, ".csv")
 cluster <- read.csv(file.path(clustDir, name))
 start <- which(names(cluster) == "bankssts")
 colnames(cluster)[start:ncol(cluster)] <- paste0(colnames(cluster)[start:ncol(cluster)], "_centile")
-# 根据脑指标对于人群聚类的贡献进行加权
-rank <- read.csv(file.path(clustDir, paste0("abide_A_asd_male_dev_Spectral_Cluster_34DK_RM_Rank_",
-                                            newDate, ".csv")))
 All <- merge(cluster, pheno, by = "participant", All.x = TRUE)
 
 
@@ -85,7 +87,12 @@ H <- subset(All, clusterID == "2")
 H <- H[, -1]
 
 
-# 给rank中的脑指标按照贡献大小赋值
+################################## Part 1: 全脑指标合成的新指标作为自变量 ##########################
+
+# 根据脑指标对于人群聚类的贡献进行加权
+rank <- read.csv(file.path(clustDir, paste0("asd_male_Spectral_Cluster_RMrank_", newDate, ".csv")))
+
+# 给rank中的脑指标按照贡献大小加权
 rank$Feature <- paste0(rank$Feature, "_centile")
 rank$Rank <- 0 # 创建一个新的列来保存排名
 # 将正数和负数分开排名
@@ -101,6 +108,7 @@ importance <- rank
 
 
 ################### L组
+
 
 # 确保 SITE_ID 和 FIQ 是因子和数值类型
 L$SITE_ID <- as.factor(L$SITE_ID)
@@ -173,7 +181,7 @@ for (name_cog in names_cog) {
 L_sorted <- arrange(results_L, p_value)
 
 ### 保存下来结果
-name <- paste0("abide_A_asd_male_dev_Spectral_Cluster_34DK_statis_CorrA_L_", newDate, ".csv")
+name <- paste0("asd_male_dev_SC_corr_Part1_34_L_", newDate, ".csv")
 write.csv(L_sorted, file.path(statiDir, name), row.names = F)
 
 
@@ -252,13 +260,13 @@ for (name_cog in names_cog) {
 H_sorted <- arrange(results_H, p_value)
 
 ### 保存下来结果
-name <- paste0("abide_A_asd_male_dev_Spectral_Cluster_34DK_statis_CorrA_H_", newDate, ".csv")
+name <- paste0("asd_male_dev_SC_corr_Part1_34_H_", newDate, ".csv")
 write.csv(H_sorted, file.path(statiDir, name), row.names = F)
 
 
 
 
-################### 筛选显著且相关大于0.2的结果，没有，因此以下省略
+################### 筛选显著p < 0.01且相关大于0.2的结果
 H_sorted <- H_sorted %>%
   filter(abs(coef) > 0.2, p_value < 0.01)
 L_sorted <- L_sorted %>%
@@ -285,29 +293,31 @@ for (i in 1:nrow(sorted)) {
   sorted[i, "Hn"] <- nrow(temp)
 }
 
-name <- paste0("abide_A_asd_male_dev_Spectral_Cluster_34DK_statis_CorrA_Final_", newDate, ".csv")
+name <- paste0("asd_male_dev_SC_corr_Part1_34_LH_", newDate, ".csv")
 write.csv(sorted[, -6], file.path(statiDir, name), row.names = F)
 
 
-##################################### Part 2: 画图 #################################################
+
+
+################### 画图
 
 
 for (i in 1:nrow(sorted)) {
   to_plot_names <- c("weighted_var", sorted[i, "name_cog"])
-
+  
   plotPoint_L <- L[, to_plot_names]
   plotPoint_L <- plotPoint_L[!is.na(plotPoint_L[[2]]), ]
   plotPoint_H <- H[, to_plot_names]
   plotPoint_H <- plotPoint_H[!is.na(plotPoint_H[[2]]), ]
-
+  
   if (nrow(plotPoint_L) < 30 & nrow(plotPoint_H) < 30) {
     next  # 如果数据点过少，跳过当前循环
   }
-
+  
   colnames(plotPoint_L)[1:2] <- c("x","y")
   colnames(plotPoint_H)[1:2] <- c("x","y")
-
-
+  
+  
   if (is.na(sorted[i, "Lp"])) {
     ggplot() +
       geom_point(data = plotPoint_L, aes(x = x, y = y, color = "lightgray"),
@@ -347,6 +357,193 @@ for (i in 1:nrow(sorted)) {
             axis.text.x = element_text(size = 15, face = "bold")) +
       scale_color_identity()
   }
-  name <- paste0("CorrA_", i, "_", to_plot_names[1], "_", to_plot_names[2], "_", newDate, ".png")
+  name <- paste0("SC_corr_Part1_34_", to_plot_names[1], "_", to_plot_names[2], "_", newDate, ".png")
   ggsave(file.path(plotDir, name), width = 7, height = 7, units = "in", dpi = 500)
 }
+
+
+
+################################## Part 2: 34个脑区中贡献top5的指标合成的新指标作为自变量 ##########
+
+# 根据脑指标对于人群聚类的贡献进行加权
+rank <- read.csv(file.path(clustDir, paste0("asd_male_Spectral_Cluster_RMrank_", newDate, ".csv")))
+
+rank <- rank[1:5,] # 选择top5
+
+# 给rank中的脑指标按照贡献大小赋值
+rank$Feature <- paste0(rank$Feature, "_centile")
+rank$Rank <- 0 # 创建一个新的列来保存排名
+# 将正数和负数分开排名
+positive_ranks <- rank(rank$Importance[rank$Importance > 0], ties.method = "first")
+negative_ranks <- rank(-rank$Importance[rank$Importance < 0], ties.method = "first")
+# 将排名结果赋值回数据框中
+rank$Rank[rank$Importance > 0] <- positive_ranks
+rank$Rank[rank$Importance < 0] <- -negative_ranks
+rank$Importance <- rank$Rank
+
+importance <- rank
+
+
+################### L组
+
+# 确保 SITE_ID 和 FIQ 是因子和数值类型
+L$SITE_ID <- as.factor(L$SITE_ID)
+L$FIQ <- as.numeric(L$FIQ)
+
+# 对 L 数据框中的 names_brain 列进行加权
+weighted_names_brain <- L %>%
+  select(all_of(importance$Feature)) %>%
+  mutate(across(everything(), ~ . * importance$Importance[match(cur_column(), importance$Feature)]))
+
+# 将加权后的列合并成一个新的自变量
+weighted_var <- rowSums(weighted_names_brain, na.rm = TRUE)
+if(length(weighted_var) != nrow(L)) {
+  stop("Length of weighted_var does not match number of rows in L")
+}
+
+# 将加权合成的新变量添加到 L 数据框中
+L$weighted_var <- weighted_var
+
+# 初始化空数据框来存储相关计算的结果
+results_L <- data.frame()
+
+# 循环 names_cog 列，计算 Spearman 相关性
+for (name_cog in names_cog) {
+  if (name_cog %in% names(L)) {
+    y <- L[[name_cog]] # 提取因变量
+    # 确保 y 中非 NA 值不少于 30 个
+    if (sum(!is.na(y)) >= 30) {
+      # 创建临时数据框，包含做相关分析需要的列，并过滤掉 FIQ 为 NA 的行
+      temp_L <- L[!is.na(y) & !is.na(L$FIQ), c("weighted_var", "SITE_ID", "FIQ", name_cog)]
+      temp_L$y <- y[!is.na(y) & !is.na(L$FIQ)]
+      
+      if (nrow(temp_L) >= 30) {
+        if (length(unique(temp_L$SITE_ID)) > 1) { # 确保 SITE_ID 还有多个水平
+          # 对 y 和 weighted_var 进行回归，控制 SITE_ID 和 FIQ
+          y_lm <- lm(y ~ SITE_ID + FIQ, data = temp_L)
+          x_lm <- lm(weighted_var ~ SITE_ID + FIQ, data = temp_L)
+          
+          # 提取残差
+          y_residuals <- residuals(y_lm)
+          x_residuals <- residuals(x_lm)
+          
+          # 使用 Spearman 相关性计算残差之间的相关性
+          cor_test <- cor.test(y_residuals, x_residuals, method = "spearman")
+        } else {
+          # 如果 SITE_ID 只有一个水平，不控制 SITE_ID，但仍然控制 FIQ
+          y_lm <- lm(y ~ FIQ, data = temp_L)
+          x_lm <- lm(weighted_var ~ FIQ, data = temp_L)
+          
+          # 提取残差
+          y_residuals <- residuals(y_lm)
+          x_residuals <- residuals(x_lm)
+          
+          # 使用 Spearman 相关性计算残差之间的相关性
+          cor_test <- cor.test(y_residuals, x_residuals, method = "spearman")
+        }
+        
+        # 保存 Spearman 相关系数和显著性到新的数据框中
+        results_L <- rbind(results_L, data.frame(
+          name_cog = name_cog,
+          coef = cor_test$estimate,
+          p_value = cor_test$p.value
+        ))
+      }
+    }
+  }
+}
+
+########### 给P值排序
+L_sorted <- arrange(results_L, p_value)
+
+### 保存下来结果
+name <- paste0("asd_male_dev_SC_corr_Part1_05_L_", newDate, ".csv")
+write.csv(L_sorted, file.path(statiDir, name), row.names = F)
+
+
+
+################### H组
+
+
+# 确保 SITE_ID 和 FIQ 是因子和数值类型
+H$SITE_ID <- as.factor(H$SITE_ID)
+H$FIQ <- as.numeric(H$FIQ)
+
+# 对 H 数据框中的 names_brain 列进行加权
+weighted_names_brain <- H %>%
+  select(all_of(importance$Feature)) %>%
+  mutate(across(everything(), ~ . * importance$Importance[match(cur_column(), importance$Feature)]))
+
+# 将加权后的列合并成一个新的自变量
+weighted_var <- rowSums(weighted_names_brain, na.rm = TRUE)
+if(length(weighted_var) != nrow(H)) {
+  stop("Length of weighted_var does not match number of rows in H")
+}
+
+# 将加权合成的新变量添加到 H 数据框中
+H$weighted_var <- weighted_var
+
+# 初始化空数据框来存储相关计算的结果
+results_H <- data.frame()
+
+# 循环 names_cog 列，计算 Spearman 相关性
+for (name_cog in names_cog) {
+  if (name_cog %in% names(H)) {
+    y <- H[[name_cog]] # 提取因变量
+    # 确保 y 中非 NA 值不少于 30 个
+    if (sum(!is.na(y)) >= 30) {
+      # 创建临时数据框，包含做相关分析需要的列，并过滤掉 FIQ 为 NA 的行
+      temp_H <- H[!is.na(y) & !is.na(H$FIQ), c("weighted_var", "SITE_ID", "FIQ", name_cog)]
+      temp_H$y <- y[!is.na(y) & !is.na(H$FIQ)]
+      
+      if (nrow(temp_H) >= 30) {
+        if (length(unique(temp_H$SITE_ID)) > 1) { # 确保 SITE_ID 还有多个水平
+          # 对 y 和 weighted_var 进行回归，控制 SITE_ID 和 FIQ
+          y_lm <- lm(y ~ SITE_ID + FIQ, data = temp_H)
+          x_lm <- lm(weighted_var ~ SITE_ID + FIQ, data = temp_H)
+          
+          # 提取残差
+          y_residuals <- residuals(y_lm)
+          x_residuals <- residuals(x_lm)
+          
+          # 使用 Spearman 相关性计算残差之间的相关性
+          cor_test <- cor.test(y_residuals, x_residuals, method = "spearman")
+        } else {
+          # 如果 SITE_ID 只有一个水平，不控制 SITE_ID，但仍然控制 FIQ
+          y_lm <- lm(y ~ FIQ, data = temp_H)
+          x_lm <- lm(weighted_var ~ FIQ, data = temp_H)
+          
+          # 提取残差
+          y_residuals <- residuals(y_lm)
+          x_residuals <- residuals(x_lm)
+          
+          # 使用 Spearman 相关性计算残差之间的相关性
+          cor_test <- cor.test(y_residuals, x_residuals, method = "spearman")
+        }
+        
+        # 保存 Spearman 相关系数和显著性到新的数据框中
+        results_H <- rbind(results_H, data.frame(
+          name_cog = name_cog,
+          coef = cor_test$estimate,
+          p_value = cor_test$p.value
+        ))
+      }
+    }
+  }
+}
+
+########### 给P值排序
+H_sorted <- arrange(results_H, p_value)
+
+### 保存下来结果
+name <- paste0("asd_male_dev_SC_corr_Part1_05_H_", newDate, ".csv")
+write.csv(H_sorted, file.path(statiDir, name), row.names = F)
+
+
+
+################### 筛选显著p < 0.01且相关大于0.2的结果
+H_sorted <- H_sorted %>%
+  filter(abs(coef) > 0.2, p_value < 0.01)
+L_sorted <- L_sorted %>%
+  filter(abs(coef) > 0.2, p_value < 0.01)
+# 【没有，因此后续省略】
--- 501_statisticCorrlate_Part2.R
+++ 501_statisticCorrlate_Part2.R
@@ -1,9 +1,14 @@
-# 本代码用来分析两组ASD男性的变量之间的相关系数和显著性水平
-# 斯皮尔曼相关，Site作为控制变量，top 5个脑指标合成的新指标作为自变量,
-# 保存原始的相关系数和p值csv文件，另外，筛选p小于0.01且r大于0.2的结果，保存csv文件
-# 之后手动筛选保存一个xlsx
-# 按照显著性水平结果，绘制相关图png
+# 本代码用来分析两组ASD发育男性的变量之间的相关系数和显著性水平
+# 使用斯皮尔曼相关，仅Site作为控制变量
 # 雪如 2024年2月28日于北师大办公室
+##################################
+# Part 1: 34个局部脑指标（centile）加权合成的新指标作为自变量
+# 保存原始的相关系数和p值csv文件，另外，筛选p小于0.01且r大于0.2的结果【无】
+
+# Part 2: 34个脑区中贡献top5的指标合成的新指标作为自变量
+# 保存原始的相关系数和p值csv文件，另外，筛选p小于0.01且r大于0.2的结果，保存csv文件
+# 按照显著性水平结果，绘制相关图png
+##################################
 
 rm(list=ls())
 packages <- c("tidyverse", "mgcv", "stringr", "reshape2", "magrittr", "ggplot2", "dplyr", "readxl",
@@ -15,30 +20,27 @@ sapply(packages, require, character.only = TRUE)
 # abideDir <- '/Volumes/Xueru/PhDproject/ABIDE' # mac
 abideDir <- 'E:/PhDproject/ABIDE' # winds
 phenoDir <- file.path(abideDir, "Preprocessed")
-clustDir <- file.path(abideDir, "Analysis/Cluster/Cluster_A/SpectralCluster34DK")
+clustDir <- file.path(abideDir, "Analysis/Cluster/SpectralCluster")
 statiDir <- file.path(abideDir, "Analysis/Statistic")
-plotDir <- file.path(abideDir, "Plot/Cluster/Cluster_A/SpectralCluster34DK/Corr/CorrE")
+plotDir <- file.path(abideDir, "Plot/Cluster/SpectralCluster/Corr")
 resDate <- "240315"
 newDate <- "240610"
 
 # 认知行为
-pheno <- read.csv(file.path(phenoDir, paste0("abide_A_All_", resDate, ".csv")))
+pheno <- read.csv(file.path(phenoDir, paste0("abide_A_all_", resDate, ".csv")))
 colnames(pheno)[which(names(pheno) == "Participant")] <- "participant"
 # 聚类信息、脑形态测量百分位数
-name <- paste0("abide_A_asd_male_dev_Spectral_Cluster_34DK_", newDate, ".csv")
+name <- paste0("asd_male_Spectral_Cluster_", newDate, ".csv")
 cluster <- read.csv(file.path(clustDir, name))
 start <- which(names(cluster) == "bankssts")
 colnames(cluster)[start:ncol(cluster)] <- paste0(colnames(cluster)[start:ncol(cluster)], "_centile")
-# 根据脑指标对于人群聚类的贡献进行加权
-rank <- read.csv(file.path(clustDir, paste0("abide_A_asd_male_dev_Spectral_Cluster_34DK_RM_Rank_",
-                                            newDate, ".csv")))
 All <- merge(cluster, pheno, by = "participant", All.x = TRUE)
 
 
 # 选择自变量列
 names_brain <- names(cluster)[3:ncol(cluster)]
 
-names_cog <- c("FIQ", "PIQ", "VIQ", "ADOS_2_SEVERITY_TOTAL", "ADOS_2_TOTAL", "ADOS_2_SOCAFFECT",
+names_cog <- c("FIQ", "VIQ", "PIQ", "ADOS_2_SEVERITY_TOTAL", "ADOS_2_TOTAL", "ADOS_2_SOCAFFECT",
                "ADOS_2_RRB", "SRS_AWARENESS_RAW", "SRS_COGNITION_RAW", "SRS_COMMUNICATION_RAW",
                "SRS_MOTIVATION_RAW", "SRS_MANNERISMS_RAW", "SRS_AWARENESS_T", "SRS_COGNITION_T",
                "SRS_COMMUNICATION_T", "SRS_MOTIVATION_T", "SRS_MANNERISMS_T", "SRS_TOTAL_T",
@@ -85,7 +87,187 @@ H <- subset(All, clusterID == "2")
 H <- H[, -1]
 
 
+################################## Part 1: 全脑指标合成的新指标作为自变量 ##########################
+
+# 根据脑指标对于人群聚类的贡献进行加权
+rank <- read.csv(file.path(clustDir, paste0("asd_male_Spectral_Cluster_RMrank_", newDate, ".csv")))
+
+# 初始化空数据框来存储相关计算的结果
+results_L <- data.frame()
+results_H <- data.frame()
+
+# 给rank中的脑指标按照贡献大小赋值
+rank$Feature <- paste0(rank$Feature, "_centile")
+rank$Rank <- 0 # 创建一个新的列来保存排名
+# 将正数和负数分开排名
+positive_ranks <- rank(rank$Importance[rank$Importance > 0], ties.method = "first")
+negative_ranks <- rank(-rank$Importance[rank$Importance < 0], ties.method = "first")
+# 将排名结果赋值回数据框中
+rank$Rank[rank$Importance > 0] <- positive_ranks
+rank$Rank[rank$Importance < 0] <- -negative_ranks
+rank$Importance <- rank$Rank
+rank <- rank[, -3]
+
+importance <- rank %>% select(Feature, Importance)
+
+################### L组
+
+# 确保 SITE_ID 是因子类型
+L$SITE_ID <- as.factor(L$SITE_ID)
+
+# 对 L 数据框中的 names_brain 列进行加权
+weighted_names_brain <- L %>%
+  select(all_of(importance$Feature)) %>%
+  mutate(across(everything(), ~ . * importance$Importance[match(cur_column(), importance$Feature)]))
+
+# 将加权后的列合并成一个新的自变量
+weighted_var <- rowSums(weighted_names_brain, na.rm = TRUE)
+if(length(weighted_var) != nrow(L)) {
+  stop("Length of weighted_var does not match number of rows in L")
+}
+
+# 将加权合成的新变量添加到 L 数据框中
+L$weighted_var <- weighted_var
+
+# 循环 names_cog 列，计算 Spearman 相关性
+for (name_cog in names_cog) {
+  if (name_cog %in% names(L)) {
+    y <- L[[name_cog]] # 提取因变量
+    # 确保 y 中非 NA 值不少于 30 个
+    if (sum(!is.na(y)) >= 30) {
+      # 创建临时数据框，包含做相关分析需要的列
+      temp_L <- L[!is.na(y), c("weighted_var", "SITE_ID", name_cog)]
+      temp_L$y <- y[!is.na(y)]
+      
+      if (nrow(temp_L) >= 30) {
+        if (length(unique(temp_L$SITE_ID)) > 1) { # 确保 SITE_ID 还有多个水平
+          # 对 y 和 weighted_var 进行回归，控制 SITE_ID
+          y_lm <- lm(y ~ SITE_ID, data = temp_L)
+          weighted_var_lm <- lm(weighted_var ~ SITE_ID, data = temp_L)
+          
+          # 提取残差
+          y_residuals <- residuals(y_lm)
+          weighted_var_residuals <- residuals(weighted_var_lm)
+          
+          # 使用 Spearman 相关性计算残差之间的相关性
+          cor_test <- cor.test(y_residuals, weighted_var_residuals, method = "spearman")
+        } else {
+          # 如果 SITE_ID 只有一个水平，不控制 SITE_ID
+          y_lm <- lm(y ~ 1, data = temp_L)
+          weighted_var_lm <- lm(weighted_var ~ 1, data = temp_L)
+          
+          # 提取残差
+          y_residuals <- residuals(y_lm)
+          weighted_var_residuals <- residuals(weighted_var_lm)
+          
+          # 使用 Spearman 相关性计算残差之间的相关性
+          cor_test <- cor.test(y_residuals, weighted_var_residuals, method = "spearman")
+        }
+        
+        # 保存 Spearman 相关系数和显著性到新的数据框中
+        results_L <- rbind(results_L, data.frame(
+          name_cog = name_cog,
+          coef = cor_test$estimate,
+          p_value = cor_test$p.value
+        ))
+      }
+    }
+  }
+}
+
+########### 给P值排序
+L_sorted <- arrange(results_L, p_value)
+
+### 保存下来结果
+name <- paste0("asd_male_dev_SC_corr_Part2_34_L_", newDate, ".csv")
+write.csv(L_sorted, file.path(statiDir, name), row.names = F)
+
+################### H组
+
+# 确保 SITE_ID 是因子类型
+H$SITE_ID <- as.factor(H$SITE_ID)
+
+# 对 H 数据框中的 names_brain 列进行加权
+weighted_names_brain <- H %>%
+  select(all_of(importance$Feature)) %>%
+  mutate(across(everything(), ~ . * importance$Importance[match(cur_column(), importance$Feature)]))
+
+# 将加权后的列合并成一个新的自变量
+weighted_var <- rowSums(weighted_names_brain, na.rm = TRUE)
+if(length(weighted_var) != nrow(H)) {
+  stop("Length of weighted_var does not match number of rows in H")
+}
+
+# 将加权合成的新变量添加到 H 数据框中
+H$weighted_var <- weighted_var
+
+# 初始化空数据框来存储相关计算的结果
+results_H <- data.frame()
+
+# 循环 names_cog 列，计算 Spearman 相关性
+for (name_cog in names_cog) {
+  if (name_cog %in% names(H)) {
+    y <- H[[name_cog]] # 提取因变量
+    # 确保 y 中非 NA 值不少于 30 个
+    if (sum(!is.na(y)) >= 30) {
+      # 创建临时数据框，包含做相关分析需要的列
+      temp_H <- H[!is.na(y), c("weighted_var", "SITE_ID", name_cog)]
+      temp_H$y <- y[!is.na(y)]
+      
+      if (nrow(temp_H) >= 30) {
+        if (length(unique(temp_H$SITE_ID)) > 1) { # 确保 SITE_ID 还有多个水平
+          # 对 y 和 weighted_var 进行回归，控制 SITE_ID
+          y_lm <- lm(y ~ SITE_ID, data = temp_H)
+          weighted_var_lm <- lm(weighted_var ~ SITE_ID, data = temp_H)
+          
+          # 提取残差
+          y_residuals <- residuals(y_lm)
+          weighted_var_residuals <- residuals(weighted_var_lm)
+          
+          # 使用 Spearman 相关性计算残差之间的相关性
+          cor_test <- cor.test(y_residuals, weighted_var_residuals, method = "spearman")
+        } else {
+          # 如果 SITE_ID 只有一个水平，不控制 SITE_ID
+          y_lm <- lm(y ~ 1, data = temp_H)
+          weighted_var_lm <- lm(weighted_var ~ 1, data = temp_H)
+          
+          # 提取残差
+          y_residuals <- residuals(y_lm)
+          weighted_var_residuals <- residuals(weighted_var_lm)
+          
+          # 使用 Spearman 相关性计算残差之间的相关性
+          cor_test <- cor.test(y_residuals, weighted_var_residuals, method = "spearman")
+        }
+        
+        # 保存 Spearman 相关系数和显著性到新的数据框中
+        results_H <- rbind(results_H, data.frame(
+          name_cog = name_cog,
+          coef = cor_test$estimate,
+          p_value = cor_test$p.value
+        ))
+      }
+    }
+  }
+}
+
+########### 给P值排序
+H_sorted <- arrange(results_H, p_value)
+
+### 保存下来结果
+name <- paste0("asd_male_dev_SC_corr_Part2_34_H_", newDate, ".csv")
+write.csv(H_sorted, file.path(statiDir, name), row.names = F)
+
+# 【p没有小于0.01的，因此没有后续】
+
+
+
+################################## Part 2: 34个脑区中贡献top5的指标合成的新指标作为自变量 ##########
+
+# 根据脑指标对于人群聚类的贡献进行加权
+rank <- read.csv(file.path(clustDir, paste0("asd_male_Spectral_Cluster_RMrank_", newDate, ".csv")))
+
 rank <- rank[1:5,] # 选择top5
+
 
 # 给rank中的脑指标按照贡献大小赋值
 rank$Feature <- paste0(rank$Feature, "_centile")
@@ -175,7 +357,7 @@ for (name_cog in names_cog) {
 L_sorted <- arrange(results_L, p_value)
 
 ### 保存下来结果
-name <- paste0("abide_A_asd_male_dev_Spectral_Cluster_34DK_statis_CorrE_L_", newDate, ".csv")
+name <- paste0("asd_male_dev_SC_corr_Part2_05_L_", newDate, ".csv")
 write.csv(L_sorted, file.path(statiDir, name), row.names = F)
 
 
@@ -253,7 +435,7 @@ for (name_cog in names_cog) {
 H_sorted <- arrange(results_H, p_value)
 
 ### 保存下来结果
-name <- paste0("abide_A_asd_male_dev_Spectral_Cluster_34DK_statis_CorrE_H_", newDate, ".csv")
+name <- paste0("asd_male_dev_SC_corr_Part2_05_H_", newDate, ".csv")
 write.csv(H_sorted, file.path(statiDir, name), row.names = F)
 
 
@@ -284,11 +466,11 @@ for (i in 1:nrow(sorted)) {
   sorted[i, "Hn"] <- nrow(temp)
 }
 
-name <- paste0("abide_A_asd_male_dev_Spectral_Cluster_34DK_statis_CorrE_Final_", newDate, ".csv")
+name <- paste0("asd_male_dev_SC_corr_Part2_05_LH_", newDate, ".csv")
 write.csv(sorted[, -6], file.path(statiDir, name), row.names = F)
 
 
-##################################### Part 2: 画图 #################################################
+################画图 
 
 
 for (i in 1:nrow(sorted)) {
@@ -345,6 +527,7 @@ for (i in 1:nrow(sorted)) {
             axis.text.x = element_text(size = 15, face = "bold")) +
       scale_color_identity()
   }
-  name <- paste0("CorrE_", i, "_", to_plot_names[1], "_", to_plot_names[2], "_", newDate, ".png")
+  name <- paste0("SC_corr_Part2_05_", to_plot_names[1], "_", to_plot_names[2], "_", newDate, ".png")
   ggsave(file.path(plotDir, name), width = 7, height = 7, units = "in", dpi = 500)
 }
+
